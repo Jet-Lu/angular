21a0a76483f97e1a2e2cda612294600e0f308098
Jet Lu <1548036276@qq.com>
2018-01-22 19:50:56 +0800

    配置路由

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    preloadingStrategy: PreloadAllModules
+  })],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
